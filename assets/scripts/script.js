ddfed492aa3ddeebc7678b6e480819dd53bf77ef
Camila Amaro <camilaamarov@yahoo.es>
2024-09-23 14:23:01 -0500

    feat/refactor: Added new code and refactored files

---
+++ assets/scripts/script.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you! We will get in touch with you soon.");
+  });
+  
